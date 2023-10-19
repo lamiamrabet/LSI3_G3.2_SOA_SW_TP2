3ab89d5b1e954c7668153328ac0d1e346cfadcdc
Lamia Mrabet <lamiamrabet456@gmail.com>
2023-10-19 20:41:19 +0100

    ex2

--- src/service/BanqueService.java
+++ src/service/BanqueService.java
@@ -1,9 +1,24 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class BanqueService {
 	
 	public double conversion (double montant) {
 		 return montant*3.36;
+	}
+	public Compte getCompte(int code) {
+		return new Compte(code, 0, new Date());
+		
+	}
+	List<Compte> getComptes(){
+		List<Compte> list = new ArrayList<>();
+		Date d = new Date(12, 01, 23);
+		Compte cmt = new Compte(0, 100.6, d);
+		list.add(cmt);
+		return list;
 	}
 
 	public static void main(String[] args) {
